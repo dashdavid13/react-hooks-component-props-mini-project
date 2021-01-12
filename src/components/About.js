8e7b86a5b4540dc5a96696f5b5872604f441ae92
dashdavid13 <davidamedina619@gmail.com>
2021-01-11 22:20:09 -0500

    done

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,10 @@ import React from "react";
 
 
 function About ({ about, image = "https://via.placeholder.com/215" }) {
+    const img = "blog logo"
   return (
     <aside>
-        <img src={image} alt="logo"></img>
+        <img src={image} alt={img}></img>
         <p>{about}</p>
     </aside>
   );
